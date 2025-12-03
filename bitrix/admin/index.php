994748ef24627e1c3b0790dcb3a79d31e2d97b89
liber87 <bd87@bk.ru>
2025-12-03 22:21:54 +0300

    Update index.php

--- bitrix/admin/index.php
+++ bitrix/admin/index.php
@@ -5,8 +5,36 @@
 	$modx->db->connect();	
 	if (empty ($modx->config)) {
 		$modx->getSettings();
-	}
+	}	
+	$auth = 'Авторизация';
+	$please = 'Пожалуйста, авторизуйтесь';
+	$login = 'Логин';
+	$password = 'Пароль';
+	$remember = 'Запомнить меня на этом компьютере';
+	$forget = 'Забыли свой пароль?';
+	$chat = 'Чат сообщества';
+	$query = 'Запрос пароля';
+	$send = 'Выслать';
+	$send_me = 'Выслать контрольную строку';
+	$error = 'Ошибка авторизации';
+	$if = 'Если вы забыли пароль, введите ваш E-Mail, указанный при регистрации. Контрольная строка для смены пароля будет выслана вам по электронной почте.';
+	$lng = 'Ru';
 	
+	if ((isset($_GET['lang'])) && ($_GET['lang']=='en')){
+		$auth = 'Authorization';
+		$please = 'Please log in';
+		$login = 'Login';
+		$password = 'Password';
+		$remember = 'Remember me';
+		$forget = 'Forgot password?';
+		$chat = 'Chat community';
+		$query = 'Password request';
+		$send = 'Send';
+		$send_me = 'Send me checkword';
+		$error = 'Error auth';
+		$if = 'If you cannot remember your password, enter your login or e-mail you have used for registration. A message containing the check word you can use to change the password will be sent to your e-mail.';
+		$lng = 'En';
+	}
 ?>
 <!DOCTYPE html>
 <html>
@@ -21,36 +49,35 @@
 		<script src="../js/main/pageobject/pageobject.js"></script>
 		<script src="../js/main/core/core_window.js"></script>
 		<script src="../js/main/core/core_admin_login.js"></script>
-		
-		
 		<title>Авторизация - <?php echo $modx->config['site_name'];?></title>
 		<script>
 			/* <![CDATA[ */
 			document.addEventListener('DOMContentLoaded', function(){
-			var form = document.getElementById('loginfrm');
-			
-			form.onsubmit = function(e) {
-				e.preventDefault();
-				
-				
-				//document.getElementById('mainloader').classList.add('show');
-				var xhr = new XMLHttpRequest();
-				xhr.open('POST', './../../<?php echo MGR_DIR;?>/processors/login.processor.php', true);
-				xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded;');
-				xhr.onload = function() {					
-					if (this.readyState === 4) {
-						var header = this.response.substr(0, 9);
-						if (header.toLowerCase() === 'location:') {
-							window.location = this.response.substr(10);
-							} else {							
-							alert(this.response);
+				var form = document.getElementById('loginfrm');
+				form.onsubmit = function(e) {
+					e.preventDefault();				
+					var xhr = new XMLHttpRequest();
+					xhr.open('POST', './../../<?php echo MGR_DIR;?>/processors/login.processor.php', true);
+					xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded;');
+					xhr.onload = function() {					
+						if (this.readyState === 4) {
+							var header = this.response.substr(0, 9);
+							if (header.toLowerCase() === 'location:') {
+								window.location = this.response.substr(10);
+								} else {
+								document.getElementById('authorize').classList.add('login-popup-error-shake');
+								document.getElementById('error-message').style.display = 'block';
+								document.getElementById('error-info').innerHTML = this.response;
+								setTimeout(function(){
+									document.getElementById('authorize').classList.remove('login-popup-error-shake');
+									document.getElementById('error-message').style.display = 'none';
+								}, 5000);
+							}
 						}
-					}
+					};
+					xhr.send('ajax=1&username=' + encodeURIComponent(form.username.value) + '&password=' + encodeURIComponent(form.password.value) + '&rememberme=' + form.rememberme.value);				
+					return false;
 				};
-				xhr.send('ajax=1&username=' + encodeURIComponent(form.username.value) + '&password=' + encodeURIComponent(form.password.value) + (form.captcha_code ? '&captcha_code=' + encodeURIComponent(form.captcha_code.value) : '') + '&rememberme=' + form.rememberme.value);
-				
-				return false;
-			};
 			});
 			/* ]]> */
 		</script>
@@ -76,7 +103,6 @@
 					url: '/bitrix/admin/'
 				});
 			</script>
-			
 			<table class="login-popup-alignment">
 				<tr>
 					<td class="login-popup-alignment-2" id="popup_alignment">
@@ -88,81 +114,67 @@
 									echo idn_to_utf8($_SERVER['SERVER_NAME']);
 								?> - <?php echo $modx->config['site_name'];?></span>
 							</a>
-							<div class="login-language-btn-wrap"><div class="login-language-btn" id="login_lang_button">RU</div></div>
+							<div class="login-language-btn-wrap"><div class="login-language-btn" id="login_lang_button"><?php echo $lng;?></div></div>
 						</div>
-						
 						<div class="login-footer">
 							<div class="login-footer-left"> 
 								© 2005-<?php echo date('Y');?> by the EVO. EVO™ is licensed under the GPL.
 							</div>
 							<div class="login-footer-right">
-								<a href="https://t.me/evo_cms" class="login-footer-link" target="_blank">Чат сообщества</a>			
+								<a href="https://t.me/evo_cms" class="login-footer-link" target="_blank"><?php echo $chat;?></a>			
 							</div>
 						</div>
 						<form  method="post" action="./../../<?php echo MGR_DIR;?>/processors/login.processor.php" class="bx-admin-auth-form" novalidate  name="loginfrm" id="loginfrm">			
-							
 							<div id="auth_form_wrapper">
 								<div class="login-main-popup-wrap login-popup-wrap" id="authorize">									
 									<div class="login-popup">
-										<div class="login-popup-title">Авторизация</div>
-										<div class="login-popup-title-description">Пожалуйста, авторизуйтесь</div>
-										
+										<div class="login-popup-title"><?php echo $auth;?></div>
+										<div class="login-popup-title-description"><?php echo $please;?></div>
 										<div class="login-popup-field">
-											<div class="login-popup-field-title">Логин</div>
+											<div class="login-popup-field-title"><?php echo $login;?></div>
 											<div class="login-input-wrap">
 												<input type="text" class="login-input" onfocus="BX.addClass(this.parentNode, 'login-input-active')" onblur="BX.removeClass(this.parentNode, 'login-input-active')" name="username" value="" tabindex="1">
 												<div class="login-inp-border"></div>
 											</div>
 										</div>
 										<div class="login-popup-field" id="authorize_password">
-											<div class="login-popup-field-title">Пароль</div>
+											<div class="login-popup-field-title"><?php echo $password;?></div>
 											<div class="login-input-wrap">
 												<input type="password" class="login-input" onfocus="BX.addClass(this.parentNode, 'login-input-active')" onblur="BX.removeClass(this.parentNode, 'login-input-active')" name="password" tabindex="2">
 												<div class="login-inp-border"></div>
 											</div>
 											<input type="submit" value="" class="login-btn-green" tabindex="4" onfocus="BX.addClass(this, 'login-btn-green-hover');" onblur="BX.removeClass(this, 'login-btn-green-hover')">
+											<div class="login-error-message-block" id="error-message" style="top: 35px; display: none; opacity: 1; width: auto;"><div class="login-error-message"><?php echo $error;?><span class="login-error-red" id="error-info"></span></div></div>
 											<div class="login-loading">
 												<img class="login-waiter" alt="" src="../panel/main/images/login-waiter.gif">
 											</div>
 										</div>
-										
-										
 										<div class="login-popup-checbox-block">
-											<input type="checkbox" class="adm-designed-checkbox" id="rememberme" name="rememberme" value="1" tabindex="3" onfocus="BX.addClass(this.nextSibling, 'login-popup-checkbox-label-active')" onblur="BX.removeClass(this.nextSibling, 'login-popup-checkbox-label-active')">
+											<input type="checkbox" class="adm-designed-checkbox" id="rememberme" name="rememberme" value="1" tabindex="3">
 											<label for="USER_REMEMBER" class="adm-designed-checkbox-label"></label>
-											<label for="USER_REMEMBER" class="login-popup-checkbox-label">Запомнить меня на этом компьютере</label>
+											<label for="USER_REMEMBER" class="login-popup-checkbox-label"><?php echo $remember;?></label>
 										</div>
-										<a class="login-popup-link login-popup-forget-pas" href="javascript:void(0)" onclick="BX.adminLogin.toggleAuthForm('forgot_password')">Забыли свой пароль?</a>
-										
+										<a class="login-popup-link login-popup-forget-pas" href="javascript:void(0)" onclick="BX.adminLogin.toggleAuthForm('forgot_password')"><?php echo $forget;?></a>
 										<div class="login-popup-field login-auth-serv-icons">
 											<a href="index.php" class="login-ss-button bitrix24net-button bitrix24net-button-ru"></a>
 										</div>										
-										
 									</div>
 								</div>
 								<script type="text/javascript">
 									BX.adminLogin.registerForm(new BX.authFormAuthorize('authorize', {url:'./../../<?php echo MGR_DIR;?>/processors/login.processor.php'}));
 								</script>
 							</div>
-							
 						</form>
 					</td>
 				</tr>
 			</table>
-			
-			
-			
 			<div id="login_variants" style="display: none;">
-				
 				<div id="forgot_password" class="login-popup-wrap-with-text">
 					<div class="login-popup-wrap login-popup-request-wrap">
-						
 						<div class="login-popup">
-							<div class="login-popup-title">Запрос пароля</div>
-							<div class="login-popup-title-description">Выслать контрольную строку</div>
+							<div class="login-popup-title"><?php echo $query;?></div>
+							<div class="login-popup-title-description"><?php echo $send_me;?></div>
 							<div class="login-popup-request-fields-wrap" id="forgot_password_fields">
-								
-								
 								<div class="login-popup-field">
 									<div class="login-popup-field-title">E-mail</div>
 									<div class="login-input-wrap">
@@ -170,19 +182,17 @@
 										<div class="login-inp-border"></div>
 									</div>
 								</div>								
-								
 							</div>
 							<div class="login-btn-wrap" id="forgot_password_message_button">
-								<a class="login-popup-link login-popup-return-auth" href="javascript:void(0)" onclick="BX.adminLogin.toggleAuthForm('authorize')">Авторизоваться</a>
-								<input type="button" value="Выслать" class="login-btn" name="send_account_info" onclick="window.location = './../../<?php echo MGR_DIR;?>/index.php?action=send_email&amp;email='+encodeURIComponent(document.getElementById('FMP-email').value);">
+								<a class="login-popup-link login-popup-return-auth" href="javascript:void(0)" onclick="BX.adminLogin.toggleAuthForm('authorize')"><?php echo $auth;?></a>
+								<input type="button" value="<?php echo $send;?>" class="login-btn" name="send_account_info" onclick="window.location = './../../<?php echo MGR_DIR;?>/index.php?action=send_email&amp;email='+encodeURIComponent(document.getElementById('FMP-email').value);">
 							</div>
 						</div>
 					</div>
 					<div class="login-popup-request-text" id="forgot_password_note">
-						Если вы забыли пароль, введите ваш логин или E-Mail, указанный при регистрации. Контрольная строка для смены пароля будет выслана вам по электронной почте.<br>
+						<?php echo $if;?><br>
 					</div>
 				</div>
-				
 				<script type="text/javascript">
 					var obForgMsg = new BX.authFormForgotPasswordMessage('forgot_password_message', {url:''}),
 					obForg = new BX.authFormForgotPassword('forgot_password', {
@@ -193,21 +203,15 @@
 					BX.adminLogin.registerForm(obForg);
 					BX.adminLogin.registerForm(obForgMsg);
 				</script>
-				
-				
 			</div>
-			
 		</div>
 		<div style="display: none;" id="window_wrapper"></div>
-		
 		<script type="text/javascript">
 			BX.ready(BX.defer(function(){
 				BX.addClass(document.body, 'login-animate');
 				BX.addClass(document.body, 'login-animate-popup');
 			}));
-			
-			new BX.COpener({DIV: 'login_lang_button', ACTIVE_CLASS: 'login-language-btn-active', MENU: [{'TEXT':'(ru) Russian','LINK':'/bitrix/admin/?lang=ru'},{'TEXT':'(en) English','LINK':'/bitrix/admin/?lang=en'}]});
+			new BX.COpener({DIV: 'login_lang_button', ACTIVE_CLASS: 'login-language-btn-active', MENU: [{'TEXT':'(ru) Русский','LINK':'/bitrix/admin/?lang=ru'},{'TEXT':'(en) English','LINK':'/bitrix/admin/?lang=en'}]});
 		</script>
-		
 	</body>
 </html>
